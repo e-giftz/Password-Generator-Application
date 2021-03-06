4795bd1c51a72f307bbcb95f7c052e1677d954b1
Gift <56089461+e-giftz@users.noreply.github.com>
2021-03-06 14:46:54 -0500

    Add index.html, style.css and script.js with starter code

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//Create arrays to hold the various characters
+var lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var UpperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var myNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "~", "?", "/", " ", "'", ",", ".",";", ":", "<", ">", "]", "[", "{", "}", "|", "`"];
+
+// Generate password using the criteria selected by user
+function generatePassword(){
+  
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
